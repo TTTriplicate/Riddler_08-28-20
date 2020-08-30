1428994e01d15386f5a1a72502022d34d7fe5dc8
TTTriplicate <tttriplicate@gmail.com>
2020-08-30 11:15:43 -0400

    starting off

---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import sphere.py #
+
+print(sphere.area(6378.001))
---
+++ sphere.py
@@ -0,0 +1,6 @@
+#!/usr.bin/python3
+
+import pi from math
+
+def area(radius):
+    return (math.pi() * 4 * (radius ** 2))
